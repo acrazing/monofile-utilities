e335cd779eaab829cb8a51e611895b6eb437d807
acrazing <joking.young@gmail.com>
2019-12-05 15:24:02 +0800

    fix(is): new is api

--- src/is.ts
+++ src/is.ts
@@ -3,49 +3,47 @@
  * @since 2018-07-30 19:05:19
  */
 
-export function isUndefined (value: any): value is void {
-  return value === undefined;
+export const isObject = (value: unknown): value is object =>
+  value === Object(value);
+export const isArray = (value: unknown): value is any[] =>
+  value instanceof Array;
+export const isDate = (value: unknown): value is Date => value instanceof Date;
+
+export const isBlob =
+  typeof Blob === 'object'
+    ? (input: unknown): input is Blob => input instanceof Blob
+    : (input: unknown): input is Blob => false;
+export const isFile =
+  typeof File === 'object'
+    ? (input: unknown): input is File => input instanceof File
+    : (input: unknown): input is File => false;
+export const isBufferSource =
+  typeof ArrayBuffer === 'object'
+    ? (input: unknown): input is BufferSource =>
+        input instanceof ArrayBuffer || ArrayBuffer.isView(input)
+    : (input: unknown): input is BufferSource => false;
+export const isFormData =
+  typeof FormData === 'object'
+    ? (input: unknown): input is FormData => input instanceof FormData
+    : (input: unknown): input is FormData => false;
+export const isURLSearchParams =
+  typeof URLSearchParams === 'object'
+    ? (input: unknown): input is URLSearchParams =>
+        input instanceof URLSearchParams
+    : (input: unknown): input is URLSearchParams => false;
+export const isReadableStream =
+  typeof ReadableStream === 'object'
+    ? (input: unknown): input is ReadableStream =>
+        input instanceof ReadableStream
+    : (input: unknown): input is ReadableStream => false;
+
+export function isBodyInit(input: unknown): input is BodyInit {
+  return (
+    typeof input === 'string' ||
+    isBlob(input) ||
+    isBufferSource(input) ||
+    isFormData(input) ||
+    isURLSearchParams(input) ||
+    isReadableStream(input)
+  );
 }
-
-export function isNull (value: any): value is null {
-  return value === null;
-}
-
-export function isObject (value: any): value is object {
-  return value === Object(value);
-}
-
-export function isArray (value: any): value is Array<any> {
-  return value instanceof Array;
-}
-
-export function isDate (value: any): value is Date {
-  return value instanceof Date;
-}
-
-export function isBlob (value: any): value is Blob {
-  return typeof Blob === 'function' && value instanceof Blob;
-}
-
-export function isFile (value: any): value is File {
-  return typeof File === 'function' && value instanceof File;
-}
-
-export function isFormData (value: any): value is FormData {
-  return value instanceof FormData;
-}
-
-export type IsTypedArray = (value: any) => value is (Int8Array | Int16Array | Int32Array | Uint8Array | Uint8ClampedArray | Uint16Array | Uint32Array | Float32Array | Float64Array)
-
-export const isTypedArray: IsTypedArray = (typeof Int8Array === 'function'
-  ? (value: any) => {
-    return value instanceof Int8Array
-      || value instanceof Int16Array
-      || value instanceof Int32Array
-      || value instanceof Uint8Array
-      || value instanceof Uint8ClampedArray
-      || value instanceof Uint16Array
-      || value instanceof Uint32Array
-      || value instanceof Float32Array
-      || value instanceof Float64Array;
-  } : () => false) as any;
