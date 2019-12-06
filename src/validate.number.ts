cc1d3ca43f04a7800a7ac1813b60deb2c22265cd
acrazing <joking.young@gmail.com>
2019-12-06 22:11:11 +0800

    feat(validate.number): add number validators

---
+++ src/validate.number.ts
@@ -0,0 +1,54 @@
+/*!
+ * Copyright 2019 acrazing <joking.young@gmail.com>. All rights reserved.
+ * @since 2019-12-06 22:08:17
+ */
+
+import { UINT32_MAX, INT32_MAX, INT32_MIN } from './consts';
+
+export function only(value: number, accepts: number[]) {
+  return accepts.indexOf(value) > -1;
+}
+
+export function gt(value: number, than: number) {
+  return value > than;
+}
+
+export function lt(value: number, than: number) {
+  return value < than;
+}
+
+export function gte(value: number, than: number) {
+  return value >= than;
+}
+
+export function lte(value: number, than: number) {
+  return value <= than;
+}
+
+export function isInt(
+  value: number,
+  gte = Number.MIN_SAFE_INTEGER,
+  lte = Number.MAX_SAFE_INTEGER,
+) {
+  return value === Math.floor(value) && value >= gte && value <= lte;
+}
+
+export function isUInt(value: number) {
+  return isInt(value, 0);
+}
+
+export function isPInt(value: number) {
+  return isInt(value, 1);
+}
+
+export function isUInt32(value: number) {
+  return isInt(value, 0, UINT32_MAX);
+}
+
+export function isUInt31(value: number) {
+  return isInt(value, 0, INT32_MAX);
+}
+
+export function isInt32(value: number) {
+  return isInt(value, INT32_MIN, INT32_MAX);
+}
